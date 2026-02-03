d542e1f44adef8c25ac984aa750cdf48c574b985
ajay <ajaykumarrj302@gmail.com>
2026-02-03 18:58:58 +0530

    made all user functions

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -28,7 +28,7 @@ export const getAllProjects = async (req:Request , res:Response) => {
             orderBy: {createdAt:'desc'}
         })
         res.json({projects})
-        
+
     } catch (error:any) {
         Sentry.captureException(error);
         res.status(500).json({message:error.code || error.message})
@@ -40,6 +40,16 @@ export const getAllProjects = async (req:Request , res:Response) => {
 export const getProjectById = async (req:Request , res:Response) => {
     try {
         
+        const {userId} = req.auth();
+        const {projectId} = req.params;
+
+        const project = await prisma.project.findUnique({
+            where:{id: projectId,userId}
+        })
+
+        if(!project) {return res.status(404).json({message:'Project not found'})}
+        res.json({project})
+
     } catch (error:any) {
         Sentry.captureException(error);
         res.status(500).json({message:error.code || error.message})
@@ -50,6 +60,27 @@ export const getProjectById = async (req:Request , res:Response) => {
 
 export const toggleProjectPublic = async (req:Request , res:Response) => {
     try {
+
+        const {userId} = req.auth();
+        const {projectId} = req.params;
+
+        const project = await prisma.project.findUnique({
+            where:{id: projectId,userId}
+        })
+
+        if(!project) {return res.status(404).json({message:'Project not found'})}
+        if(!project?.generatedImage && !project?.generatedVideo){
+            return res.status(404).json({message:'image or video not generated'})
+        }
+
+        await prisma.project.update({
+            where: {id:projectId},
+            data: {isPublished: !project.isPublished}
+        })
+
+
+    
+        res.json({isPublished : !project.isPublished})
         
     } catch (error:any) {
         Sentry.captureException(error);
